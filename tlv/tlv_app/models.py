d4af3e13bd7e47c68aa239d25cf696bcdf0b79c7
Radhika <radhika@cs.iitr.ac.in>
2020-06-11 16:23:52 +0530

    Change the DB Schema
    -> Add three dummy model classes with required schema
    -> Add validators for latitude and longitude
    -> Add test for one class
    -> Update requirements.txt

--- tlv/tlv_app/models.py
+++ tlv/tlv_app/models.py
@@ -1,26 +1,67 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django_mysql.models import JSONField, Model
 
 
-class Project(Model):
+class Covid(Model):
     """
     This class has a dummy Project model.
     """
     latitude = models.DecimalField(
-        max_digits=5,
-        decimal_places=2,
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(-90), MaxValueValidator(90)]
     )
     longitude = models.DecimalField(
-        max_digits=6,
-        decimal_places=2,
+        max_digits=10,
+        decimal_places=6,
+        validators=[MinValueValidator(-180), MaxValueValidator(180)]
     )
     time = models.DateField()
-    shop = JSONField(
-        null=True
+    category = models.CharField(
+        max_length=20
     )
-    hospital = JSONField(
-        null=True
+    entity = JSONField()
+
+
+class Disasters(Model):
+    """
+    This class has a dummy Project model.
+    """
+    latitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(-90), MaxValueValidator(90)]
     )
-    transport_station = JSONField(
-        null=True
+    longitude = models.DecimalField(
+        max_digits=10,
+        decimal_places=6,
+        validators=[MinValueValidator(-180), MaxValueValidator(180)]
     )
+    time = models.DateField()
+    category = models.CharField(
+        max_length=20
+    )
+    entity = JSONField()
+
+
+class Shops(Model):
+    """
+    This class has a dummy Project model.
+    """
+    latitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(-90), MaxValueValidator(90)]
+    )
+    longitude = models.DecimalField(
+        max_digits=10,
+        decimal_places=6,
+        validators=[MinValueValidator(-180), MaxValueValidator(180)]
+    )
+    time = models.DateField()
+    category = models.CharField(
+        max_length=20
+    )
+    entity = JSONField()
+
