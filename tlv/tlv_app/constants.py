681d6be95e1379f604b1afdcd37bef086514a2d6
Radhika <radhika@cs.iitr.ac.in>
2020-06-14 21:24:47 +0530

    Refactor backend code

---
+++ tlv/tlv_app/constants.py
@@ -0,0 +1,48 @@
+APP_NAME = 'tlv_app'
+
+CLASSES = [
+    "Covid",
+    "Disasters",
+    "Shops"
+]
+
+PRIMARY_FILTERS = [
+    ["Mild", "Medium", "Severe"],
+    ["cyclone", "earthquake", "temperature_rise", "GHGemissions"],
+    ["shops", "covid_cases", "shops_closed", "medical_centres"],
+]
+
+SECONDARY_FILTERS = [
+    {
+        "Mild": ["reported", "unreported"],
+        "Medium": ["symptomatic", "asymptomatic"],
+        "Severe": ["recovered", "dead"]
+    },
+    {
+        "cyclone": ["super_cyclonic_storm", "severe_cyclonic_storm", "cyclonic_storm", "deep_depression"],
+        "earthquake": ["count"],
+        "temperature_rise": ["value"],
+        "GHGemissions": ["CO2", "SO2", "NO"]
+    },
+    {
+        "shops": ["grocery", "stationary", "bakery", "salon"],
+        "covid_cases": ["count"],
+        "shops_closed": ["grocery", "stationary", "bakery", "salon"],
+        "medical_centres": ["private", "government"]
+    }
+]
+
+FILTERS = [
+    {
+        "primaryFilters": PRIMARY_FILTERS[0],
+        "secondaryFilters": SECONDARY_FILTERS[0]
+    },
+    {
+        "primaryFilters": PRIMARY_FILTERS[1],
+        "secondaryFilters": SECONDARY_FILTERS[1]
+    },
+    {
+        "primaryFilters": PRIMARY_FILTERS[2],
+        "secondaryFilters": SECONDARY_FILTERS[2]
+    }
+]
--- tlv/tlv_app/serializers.py
+++ tlv/tlv_app/serializers.py
@@ -3,14 +3,18 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class FilterDataSerializer(serializers.Serializer):
-  latitude = serializers.DecimalField(
+    """
+    This class serializes the filtered data
+    in appropriate format
+    """
+    latitude = serializers.DecimalField(
         max_digits=9,
         decimal_places=6,
         validators=[MinValueValidator(-90), MaxValueValidator(90)]
     )
-  longitude = serializers.DecimalField(
+    longitude = serializers.DecimalField(
         max_digits=10,
         decimal_places=6,
         validators=[MinValueValidator(-180), MaxValueValidator(180)]
     )
-  time = serializers.DateField()
+    time = serializers.DateField()
