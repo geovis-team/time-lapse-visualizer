cee5a0e54cda309fda12e3fd5951f6e1eca40050
Radhika <radhika@cs.iitr.ac.in>
2020-06-14 22:02:25 +0530

    Reformat frontend code

--- tlv_app/frontend/src/components/LoginSignupPage.js
+++ tlv_app/frontend/src/components/LoginSignupPage.js
@@ -1,47 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 // import { Menu, Segment, Button, Dropdown} from 'semantic-ui-react'
-import {Container,Col, Row, Tabs, Tab} from 'react-bootstrap'
+import { Container, Col, Row, Tabs, Tab } from 'react-bootstrap'
 
 import styles from '../static/css/LoginSignupPage.module.css'
-import {SignupForm, LoginForm} from './LoginSignupForms'
+import { SignupForm, LoginForm } from './LoginSignupForms'
 import NavigationBar from './NavigationBar'
 import Footer from './Footer'
 
-export default class LoginSignup extends Component{
-    
-    constructor(props){
-        super(props);
-        this.state = { activeItem: 'home' }
-    }
-    handleItemClick = (e,{ name }) => this.setState({ activeItem: name })
+export default class LoginSignup extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { activeItem: 'home' }
+  }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
-    render() {
-        return (
-            <div className={styles.body}>
-            <NavigationBar />
-            <Container fluid className={styles.container}>
-                <Container>
-                <Row>  
-                    <Col md> </Col>
-                    <Col sm className={styles.column} style={{padding:70}}>
-                        <Tabs defaultActiveKey="Signup" id="uncontrolled-tab-example">
-                            <Tab eventKey="Login" title="Login">
-                                <LoginForm />
-                            </Tab>
-                            <Tab eventKey="Signup" title="Signup">
-                                <SignupForm />
-                            </Tab>
-                        </Tabs>
-                    </Col>
-                    <Col md> </Col>
-                </Row>
-                </Container>
-            </Container>
-            <Footer />
-            </div>
-        )
-    }
-    
+  render () {
+    return (
+      <div className={styles.body}>
+        <NavigationBar />
+        <Container fluid className={styles.container}>
+          <Container>
+            <Row>
+              <Col md> </Col>
+              <Col sm className={styles.column} style={{ padding: 70 }}>
+                <Tabs defaultActiveKey='Signup' id='uncontrolled-tab-example'>
+                  <Tab eventKey='Login' title='Login'>
+                    <LoginForm />
+                  </Tab>
+                  <Tab eventKey='Signup' title='Signup'>
+                    <SignupForm />
+                  </Tab>
+                </Tabs>
+              </Col>
+              <Col md> </Col>
+            </Row>
+          </Container>
+        </Container>
+        <Footer />
+      </div>
+    )
+  }
 }
-
-
