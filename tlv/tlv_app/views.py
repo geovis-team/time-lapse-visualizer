cff38cc227d8f1f128833126c2361ef0caf1e11e
Radhika <radhika@cs.iitr.ac.in>
2020-06-15 09:31:43 +0530

    Add HTTP status to response

--- tlv/tlv_app/views.py
+++ tlv/tlv_app/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK, HTTP_404_NOT_FOUND
 from rest_framework.decorators import api_view
 from django.apps import apps
 
@@ -14,14 +15,27 @@ def get_filters(request, *args, **kwargs):
     :param request: get request containing model name in params
     :return: filters
     """
-    model = request.GET['model']
+    model = request.GET.get('model', None)
+    if model is None:
+        return Response(
+            status=HTTP_400_BAD_REQUEST,
+            data="Model name not given in params"
+        )
     if model == CLASSES[0]:
-        return Response(FILTERS[0])
+        filters = FILTERS[0]
     elif model == CLASSES[1]:
-        return Response(FILTERS[1])
+        filters = FILTERS[1]
     elif model == CLASSES[2]:
-        return Response(FILTERS[2])
-    return Response({"filters": []})
+        filters = FILTERS[2]
+    else:
+        return Response(
+            status=HTTP_404_NOT_FOUND,
+            data="Model with the given name not found"
+        )
+    return Response(
+        status=HTTP_200_OK,
+        data=filters
+    )
 
 
 @api_view(['GET'])
@@ -33,6 +47,11 @@ def filter_data(request, *args, **kwargs):
     :return: filtered data in appropriate format
     """
     model_name = request.GET.get('model', None)
+    if model_name is None:
+        return Response(
+            status=HTTP_400_BAD_REQUEST,
+            data="Model name not passed in params"
+        )
     filters = request.GET.getlist('filters')
     data = {}
     if model_name == CLASSES[0]:
@@ -42,15 +61,15 @@ def filter_data(request, *args, **kwargs):
     elif model_name == CLASSES[2]:
         subtypes = SECONDARY_FILTERS[2]
     else:
-        return Response({"primaryFilters": [], "data": {}})
-    for x in subtypes.keys():
-        print(x)
-    for x in filters:
-        print(x)
-    print(subtypes.keys())
-    print(filters)
+        return Response(
+            status=HTTP_404_NOT_FOUND,
+            data="Model with given name does not exist"
+        )
     if not all(x in subtypes.keys() for x in filters):
-        return Response({"primaryFilters": [], "data": {}})
+        return Response(
+            status=HTTP_400_BAD_REQUEST,
+            data="Filters specified do not exist for the given model"
+        )
 
     model = apps.get_model(app_label=APP_NAME, model_name=model_name)
     for filter in filters:
@@ -60,4 +79,7 @@ def filter_data(request, *args, **kwargs):
                                                                                              "time")
             serializer = FilterDataSerializer(q, many=True)
             data[filter][subtype] = serializer.data
-    return Response({"primaryFilters": filters, "data": data})
+    return Response(
+        status=HTTP_200_OK,
+        data={"primaryFilters": filters, "data": data}
+    )
