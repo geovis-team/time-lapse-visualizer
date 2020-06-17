f9911ad383015696332271ac4644de311e8026ee
Mallika <mallika2011@yahoo.com>
2020-06-17 18:53:56 +0530

    Integrate backend and frontend + minor changes
    - Set timeslider according to backend dataset
    - Include onClick pop-ups for map markers
    - Enable usage of primary and secondary checkboxes

--- tlv_app/frontend/src/components/MapView.js
+++ tlv_app/frontend/src/components/MapView.js
@@ -1,29 +1,77 @@
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react'
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react'
 import React, { Component, useEffect } from 'react'
 import styles from '../static/css/Visualisation.module.css'
 import PropTypes from 'prop-types'
 import MarkerClusterer from 'node-js-marker-clusterer'
 
+const onMarkerClick = (props, marker, e) => {
+  console.log('in marker click')
+  // this.setState({
+  //   selectedPlace: props,
+  //   activeMarker: marker,
+  //   showingInfoWindow: true
+  // })
+}
+
 const MarkerCluster = props => {
   const { map, google, markers } = props
   useEffect(() => {
     if (map && markers) {
+      var infoWin = new google.maps.InfoWindow()
+
       const mapMarkers = markers.map(marker => {
         const entry = new google.maps.Marker({
           position: {
-            lat: marker.props.position.lat,
-            lng: marker.props.position.lng
+            lat: marker.props.markerParams.lat,
+            lng: marker.props.markerParams.lng
           },
           map: map,
-          name: marker.props.position.type
+          name: marker.props.markerParams.type
+        })
+        google.maps.event.addListener(entry, 'click', function (evt) {
+          var content = ''
+          // TODO : Can change content formatting and styling if required
+          for (var primary in marker.props.markerParams.obj) {
+            content += '<h5>' + primary + '</h5>'
+            content +=
+              '<p>' +
+              JSON.stringify(marker.props.markerParams.obj[primary]) +
+              '</p>'
+            content += '<br /><br />'
+          }
+          infoWin.setContent(content)
+          infoWin.open(map, entry)
         })
         return entry
       })
 
       const clusterer = new MarkerClusterer(map, mapMarkers, {
+        zoomOnClick: false,
         imagePath:
           'https://raw.githubusercontent.com/googlemaps/js-marker-clusterer/gh-pages/images/m'
       })
+      google.maps.event.addListener(clusterer, 'clusterclick', function (
+        cluster
+      ) {
+        infoWin.close()
+        console.log('checking why no if', map.getZoom(), map.maxZoom)
+        if (map.getZoom() < map.maxZoom) {
+          map.setCenter(cluster.center_)
+
+          map.setZoom(map.getZoom() + 2)
+        } else {
+          var content = '(' + cluster.getMarkers().length + ')'
+          content += cluster.getMarkers().map(cl => cl.title)
+          var info = new google.maps.MVCObject()
+          info.set('position', cluster.center_)
+          infoWin.setContent(content)
+          infoWin.open(map, info)
+        }
+      })
+      google.maps.event.addListener(map, 'zoom_changed', function () {
+        infoWin.close()
+      })
+
       return () => {
         clusterer.clearMarkers()
       }
@@ -51,34 +99,54 @@ class MapView extends Component {
     super(props)
     this.state = {
       data: props.map.data,
-      time: props.map.time
+      time: props.map.time,
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
     }
+    this.onMapClick = this.onMapClick.bind(this)
   }
 
   static getDerivedStateFromProps (nextProps) {
     return {
-      time: nextProps.map.time
+      time: nextProps.map.time,
+      data: nextProps.map.data
     }
   }
   placeMarkers = () => {
-    const data = this.state.data.locations
+    const data = this.state.data.data
     const month = this.state.time
+    console.log('data.to plot', data)
+    console.log('in month', month)
     const markers = data
-      .filter(rows => rows.month == month)
+      .filter(rows => rows.time.slice(0, 7) === month.slice(0, 7))
       .map((mydata, index) => {
+        console.log('placing marker', mydata.filter)
         return (
           <Marker
             key={index}
             id={index}
-            position={{
-              lat: mydata.lat,
-              lng: mydata.lng
+            markerParams={{
+              lat: parseFloat(mydata.latitude),
+              lng: parseFloat(mydata.longitude),
+              obj: mydata.filter
             }}
           />
         )
       })
     return markers
   }
+
+  onMapClick = props => {
+    console.log('in map click')
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  }
+
   render () {
     return (
       <>
@@ -86,15 +154,18 @@ class MapView extends Component {
           <Map
             google={this.props.google}
             zoom={4}
+            maxZoom={18}
             style={mymap}
             initialCenter={{ lat: 25, lng: 80 }}
+            onClick={this.onMapClick}
           >
             <MarkerCluster
+              onClick={onMarkerClick()}
               markers={this.placeMarkers()}
-              click={this.onMarkerClick}
               mouseover={this.onMouseOver}
               mouseout={this.onMouseOut}
             />
+            {/* {this.placeMarkers()} */}
           </Map>
         </div>
       </>
